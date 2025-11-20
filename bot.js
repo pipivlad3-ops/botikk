cc0a68ff5f52b5bf460769994525ea4a18a37dbf
pipivlad3-ops <pipivlad3@gmail.com>
2025-11-20 21:11:09 +0300

    Set up Express server for bot

--- bot.js
+++ bot.js
@@ -1,3 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
+
+
+
 const TelegramBot = require('node-telegram-bot-api');
 
 // =============================
